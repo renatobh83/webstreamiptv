8475d85a3d2135a89d0b1441b7acd9062a5d4b19
Renato <neo_rlm@hotmail.com>
2025-05-08 12:51:17 -0300

    fir

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
+const app = express();
+
+
+// Caminho para ffmpeg.exe dentro do seu projeto
+const ffmpegPath = path.join(__dirname, 'ffmpeg', 'bin', 'ffmpeg.exe');
+ffmpeg.setFfmpegPath(ffmpegPath);
+// Configurar FFmpeg
+// ffmpeg.setFfmpegPath(ffmpegPath);
+
+// Configurar EJS como view engine
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Lista de streams disponíveis
+const streams = [
+  { name: 'Câmera 1', url: 'http://principal.maxpainel.net:2052/md147a/abc126ad/236' ,id: 0 },
+  { name: 'Câmera 2', url: 'http://principal.maxpainel.net:2052/md147a/abc126ad/50312', id: 1 },
+  // Adicione mais streams conforme necessário
+];
+
+// Rota principal
+app.get('/', (req, res) => {
+  res.render('index', { streams });
+});
+
+// Rota para exibir um stream específico
+app.get('/stream/:id', (req, res) => {
+    const stream = streams[req.params.id];
+    console.log(stream)
+  if (!stream) return res.status(404).send('Stream não encontrado');
+  res.render('player', { stream });
+});
+
+// Rota para o stream HTTP (transcodificação)
+app.get('/live/:id', (req, res) => {
+  const streamUrl = streams[req.params.id]?.url;
+
+  if (!streamUrl) return res.status(404).send('Stream não encontrado');
+
+  res.writeHead(200, {
+    'Content-Type': 'video/mp4',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Transfer-Encoding': 'chunked'
+  });
+
+  ffmpeg()
+  .input(streamUrl)
+  .outputOptions([
+    '-f', 'mp4',
+    '-movflags', 'frag_keyframe+empty_moov'
+  ])
+  .on('start', cmd => console.log('FFmpeg iniciado:', cmd))
+  .on('error', err => {
+    console.error('Erro no FFmpeg:', err.message);
+    res.end();
+  })
+  .on('end', () => console.log('Stream finalizado'))
+  .pipe(res, { end: true });
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
